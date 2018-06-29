c48c4533f682821e5278857b2bf6cd1f1b47d08a
VasilyBulgakov <vasiliy.bulgakov@brainymx.tech>
2018-06-29 18:51:27 +0700

    2

--- Assets/DoScan.cs
+++ Assets/DoScan.cs
@@ -9,6 +9,7 @@ public class DoScan : MonoBehaviour {
 	public static OnAllScanned scannedAllPOIs;
 
 	public GameObject target;
+	public float radius = 2f;
 
 	public GameObject debugSphere;
 	public GameObject debugSpheres;
@@ -36,25 +37,35 @@ public class DoScan : MonoBehaviour {
 		var s = new GUIStyle();
 		s.fontSize = 20;
 		if (GUI.Button(new Rect(10, Screen.height - 250, 150, 130), "ScanCenter", s))
-            scan();
+            scan(Vector3.zero);
 	}
 
-	public float radius = 0.5f;
-
-	
 
 
-	private void scan()
+	private void FixedUpdate() {
+		if(Input.touchCount > 0)
+		{
+			var touch = Input.GetTouch(0);			
+			if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+			{
+				scan(touch.position);
+			}
+		}
+		else
+		{
+			scan(new Vector3(Screen.width/2, Screen.height/2, 10));
+		}
+	}
+
+
+	private void scan(Vector3 screenPos)
 	{
-
 		Vector3 hit;
-		if( !getHitFromScene(out hit) && !getHitFromARkit(out hit) )
+		if( !getHitFromScene(screenPos, out hit) && !getHitFromARkit(screenPos, out hit) )
 		{
 			Destroy(instanceSphere);
 			foreach(var s in instanceSpheres)
 				Destroy(s);
-
-			radius = 0.5f;
 		}     
 
 		Debug.DrawLine(Camera.main.transform.position, hit,  Color.red, 1);
@@ -62,15 +73,9 @@ public class DoScan : MonoBehaviour {
 		if(instanceSphere == null)
 		{			
 			instanceSphere = GameObject.Instantiate(debugSphere, hit, Quaternion.identity);
-		}
+		}		
 		else
-		{
-			if( radius < 2 && sqrDist(hit, instanceSphere.transform.position) < Mathf.Pow(radius, 2) )
-			{
-				radius+=0.2f;				
-			}
 			instanceSphere.transform.position = hit;
-		}
 		
 		instanceSphere.transform.localScale = new Vector3(radius*2, radius*2, radius*2);
 
@@ -85,10 +90,10 @@ public class DoScan : MonoBehaviour {
 		}		
 	}
 
-	private bool getHitFromARkit(out Vector3 hitPos)
+	private bool getHitFromARkit(Vector3 screenPos, out Vector3 hitPos)
 	{
 		hitPos = Vector3.zero;
-		var screenPosition = Camera.main.ScreenToViewportPoint( new Vector3(Screen.width/2, Screen.height/2, 0.5f) );
+		var screenPosition = Camera.main.ScreenToViewportPoint( screenPos );
 		ARPoint point = new ARPoint { x = screenPosition.x, y = screenPosition.y };
 
 		var resType = ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent;
@@ -105,10 +110,11 @@ public class DoScan : MonoBehaviour {
 		return false;
 	}
 
-	private bool getHitFromScene(out Vector3 hitPos)
+	private bool getHitFromScene(Vector3 screenPos, out Vector3 hitPos)
 	{
 		RaycastHit hit;
-		if( Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward, out hit, 30.0f, 1 << 8))
+		Ray ray = Camera.main.ScreenPointToRay (screenPos);
+		if( Physics.Raycast(ray, out hit, 30.0f, 1 << 8))
 		{
 			hitPos = hit.point;
 			return true;
@@ -132,11 +138,5 @@ public class DoScan : MonoBehaviour {
 	private float sqrDist(Vector3 a, Vector3 b)
 	{		
 		return (Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2) + Mathf.Pow(a.z - b.z, 2));
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-		
 	}
 }
