a0e5f77852f50fd7ace211dac5e4f77c6cc5ecc2
VasilyBulgakov <vasiliy.bulgakov@brainymx.tech>
2018-07-11 14:26:19 +0700

    scene manager

--- Assets/TestSceneManager.cs
+++ Assets/TestSceneManager.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class ScenesList : MonoBehaviour {
-
-
+public class TestSceneManager : MonoBehaviour {
 	public UnityEngine.UI.Button buttonPrefab;
+
+	public GameObject contentContainer;
+
+	// Use this for initialization
+
 
 	// Use this for initialization
 	void Start () {
+		makeButtons();
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+
+	
+
+	private void makeButtons()
+	{
 		RectTransform thisTr = GetComponent<RectTransform>();
 		float btnHeight = buttonPrefab.GetComponent<RectTransform>().sizeDelta.y;
 		
@@ -19,12 +31,18 @@ public class ScenesList : MonoBehaviour {
 			var btn = GameObject.Instantiate(buttonPrefab);	
 			var btnTr = btn.GetComponent<RectTransform>();
 
-			btnTr.parent = thisTr;	
+			btnTr.parent = contentContainer.transform;	
 			btnTr.localPosition = new Vector3(0, -50 - 1.1f * i * btnHeight, 0);
 
 			string path = SceneUtility.GetScenePathByBuildIndex(i);
-			btn.GetComponentInChildren<UnityEngine.UI.Text>().text = path.Substring(path.LastIndexOf("/")+1);			
+			btn.GetComponentInChildren<UnityEngine.UI.Text>().text = path.Substring(path.LastIndexOf("/")+1);						
 		}		
 	}
 
+
+	
+	public void restartCurrentScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
 }
