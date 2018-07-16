1805bae4e66ca89a6c989d1f0019632bfa9a9b22
VasilyBulgakov <vasiliy.bulgakov@brainymx.tech>
2018-07-16 18:34:45 +0700

    5

---
+++ Assets/rotateMesh.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class rotateMesh : MonoBehaviour {
+
+	
+	public Transform theObject;
+	private Mesh theMesh;
+	private Vector3[] originalVerts;
+	private Vector3[] rotatedVerts;
+	public float rotatedAngleY  = 0.0f;
+ 
+ void Start() 
+ {
+     if (!theObject)
+     {
+         theObject = this.transform;
+     }
+     theMesh = theObject.GetComponent<MeshFilter>().mesh as Mesh;
+     
+     originalVerts = new Vector3[ theMesh.vertices.Length ];
+     originalVerts = theMesh.vertices;
+     
+     rotatedVerts = new Vector3[ originalVerts.Length ];
+
+	 RotateMesh();
+ }
+	
+	void Update() 
+	{
+	}
+	
+	void RotateMesh() 
+	{
+		Quaternion qAngle  = Quaternion.AngleAxis( rotatedAngleY, Vector3.up );
+		for (int vert  = 0; vert < originalVerts.Length; vert ++)
+		{
+			rotatedVerts[vert] = qAngle * originalVerts[vert];
+		}
+		
+		theMesh.vertices = rotatedVerts;
+	}
+}
